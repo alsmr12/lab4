454da20bd138a7b51809cf2ee4ba3ec2dad097e8
alsmr12 <aleksejsmirnov183@gmail.com>
2022-12-01 22:23:05 +0300

    main.c

--- pr1/main.c
+++ pr1/main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <malloc.h>
 #include <stdlib.h>
 #include "func.h"
 #include <string.h>
 #include <readline/readline.h>
-#include <readline/history.h>
 
 int main() {
-   // int t;
+    int t;
     char *str = readline("> ");
-//    t = str_input(&str);
-//    t = str_func(&str);
+    t = str_func(&str);
 //  t = str_insert(&str, '0', 0);
     printf("\n%s", str);
     free(str);
